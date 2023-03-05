368368747653afb30e938ca03546d5f268737cb6
José Pérez Rodríguez <josperrod9@alum.us.es>
2023-03-05 13:35:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Attention-A-Lightweight-2D-Hand-Pose-Estimation-Approach-Pytorch
+
+TensorFlow 版本：https://github.com/hanchenchen/Attention-A-Lightweight-2D-Hand-Pose-Estimation-Approach-master/tree/dev
+
+#### 主要任务
+在手部关键点检测任务中，使用论文 **Attention! A Lightweight 2D Hand Pose Estimation Approach**  中提出的Attention Augmented Inverted Bottleneck Block等结构。
+
+#### 测评环境
+
+- Ubuntu 16.04.6 LTS
+- Python 3.9.1
+- Cuda 10.1
+
+#### Update
+1. corrected the number of parameters
+2. Download the pre-processed dataset according to: https://github.com/HowieMa/NSRMhand. 
+    ```
+    mv path/to/CMUhand.tar ./
+    tar -xvf CMUhand.tar
+    ```
+
+#### Reference
+
+[1] Santavas N, Kansizoglou I, Bampis L, et al. Attention! a lightweight 2d hand pose estimation approach[J]. IEEE Sensors Journal, 2020. [[code]][https://github.com/nsantavas/Attention-A-Lightweight-2D-Hand-Pose-Estimation-Approach]
+
+[2] Chen Y, Ma H, Kong D, et al. Nonparametric structure regularization machine for 2D hand pose estimation[C]//Proceedings of the IEEE/CVF Winter Conference on Applications of Computer Vision. 2020: 381-390. [code][https://github.com/HowieMa/NSRMhand] (This project is manually forked from this project. )
+
+[3] Wei S E, Ramakrishna V, Kanade T, et al. Convolutional pose machines[C]//Proceedings of the IEEE conference on Computer Vision and Pattern Recognition. 2016: 4724-4732.
+
+[4] Simon T, Joo H, Matthews I, et al. Hand keypoint detection in single images using multiview bootstrapping[C]//Proceedings of the IEEE conference on Computer Vision and Pattern Recognition. 2017: 1145-1153. [Panoptic][http://domedb.perception.cs.cmu.edu/handdb.html]
+
+[5] Zimmermann C, Ceylan D, Yang J, et al. Freihand: A dataset for markerless capture of hand pose and shape from single rgb images[C]//Proceedings of the IEEE/CVF International Conference on Computer Vision. 2019: 813-822. [FreiHAND][https://lmb.informatik.uni-freiburg.de/projects/freihand/]
+
+[6] Zhang J, Jiao J, Chen M, et al. 3d hand pose tracking and estimation using stereo matching[J]. arXiv preprint arXiv:1610.07214, 2016. [SHP]
+
+[7] Shivakumar S H, Oberweger M, Rad M, et al. HO-3D: A Multi-User, Multi-Object Dataset for Joint 3D Hand-Object Pose Estimation[J]. arXiv. org e-Print archive, 2019. [HO3D_v2]
+
+"# TFG-DENSENET" 
---
+++ main.py
@@ -0,0 +1,225 @@
+import sys
+import os
+currentUrl = os.path.dirname(__file__)
+parentUrl = os.path.abspath(os.path.join(currentUrl, os.pardir))
+sys.path.append(parentUrl)
+# NSRMhand-master
+import shutil
+import argparse
+import json
+import gc
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import model
+import dataset
+from src.utils import set_logger, update_lr, get_pck_with_sigma, get_pred_coordinates, save_images, save_limb_images
+from src import loss
+from easydict import EasyDict as edict
+# ***********************  Parameter  ***********************
+
+args = edict({
+    "config_file": 'data_sample/Panoptic_base.json',
+    "GPU": 0
+})
+configs = json.load(open(args.config_file))
+
+target_sigma_list = [0.05, 0.1, 0.15, 0.2, 0.25]
+select_sigma = 0.2
+
+model_name = 'EXP_' + configs["name"]
+torch.cuda.empty_cache()
+save_dir = os.path.join(model_name, 'checkpoint/')
+test_pck_dir = os.path.join(model_name, 'test/')
+'''
+if os.path.exists(model_name):
+    print("the log directory exists!")
+    exit()
+os.makedirs(save_dir, exist_ok=True)
+os.makedirs(test_pck_dir, exist_ok=True)
+
+shutil.copy(args.config_file, model_name)
+'''
+# training parameters ****************************
+data_root = configs["data_root"]
+learning_rate = configs["learning_rate"]
+batch_size = configs["batch_size"]
+epochs = configs["epochs"]
+
+# data parameters ****************************
+
+device_ids = [args.GPU]      # multi-GPU
+torch.cuda.set_device(device_ids[0])
+cuda = False
+
+
+logger = set_logger(os.path.join(model_name, 'train.log'))
+logger.info("************** Experiment Name: {} **************".format(model_name))
+
+# ******************** build model ********************
+logger.info("Create Model ...")
+
+model = model.light_Model(configs)
+if cuda:
+    model = model.cuda(device_ids[0])
+    model = nn.DataParallel(model, device_ids=device_ids)
+# print(model)
+# from thop import profile
+# input = torch.randn(1, 3, 224, 224)
+# flops, params = profile(model, inputs=(input, ))
+# print(flops, params)
+def get_parameter_number(model):
+    total_num = sum(p.numel() for p in model.parameters())
+    trainable_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    # for name, p in model.named_parameters():
+    #     if p.requires_grad:
+    #         print(name, p.numel())
+    return {'Total': total_num, 'Trainable': trainable_num}
+print('the number of params:', get_parameter_number(model))
+
+# ******************** data preparation  ********************
+my_dataset = getattr(dataset, configs["dataset"])
+train_data = my_dataset(data_root=data_root, mode='train')
+valid_data = my_dataset(data_root=data_root, mode='valid')
+test_data = my_dataset(data_root=data_root, mode='test')
+logger.info('Total images in training data is {}'.format(len(train_data)))
+logger.info('Total images in validation data is {}'.format(len(valid_data)))
+logger.info('Total images in testing data is {}'.format(len(test_data)))
+
+train_loader = DataLoader(train_data, batch_size=8, shuffle=True)
+valid_loader = DataLoader(valid_data, batch_size=8, shuffle=False)
+test_loader = DataLoader(test_data, batch_size=8, shuffle=False)
+
+
+# ********************  ********************
+optimizer = optim.Adam(params=model.parameters(), lr=learning_rate)
+# optimizer = optim.SGD(model.parameters(), lr = learning_rate, momentum=0.0)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=3, threshold=0.00001, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08, verbose=True)
+
+def train():
+    logger.info('\nStart training ===========================================>')
+    best_epo = -1
+    max_pck = -1
+    cur_lr = learning_rate
+
+    logger.info('Initial learning Rate: {}'.format(learning_rate))
+
+    for epoch in range(1, epochs + 1):
+        logger.info('Epoch[{}/{}] ==============>'.format(epoch, epochs))
+        model.train()
+        train_label_loss = []
+
+        for step, (img, label_terget, img_name, w, h) in enumerate(train_loader):
+            # *************** target prepare ***************
+            if cuda:
+                img = img.cuda()
+                label_terget = label_terget.cuda()
+            # logger.info('LearningRate: {}'.format(optimizer.param_groups[0]['lr']))
+            optimizer.zero_grad()
+            label_pred = model(img)
+            # limb_pred (FloatTensor.cuda) size:(bz,3,C,46,46)  after sigmoid
+            # cm_pred   (FloatTensor.cuda) size:(bz,3,21,46,46)
+
+            # *************** calculate loss ***************
+
+            label_loss = loss.sum_mse_loss(label_pred.float(), label_terget.float())     # keypoint confidence loss
+            label_loss.backward()
+            optimizer.step()
+
+            train_label_loss.append(label_loss.item())
+
+            logger.info('STEP: {}  LOSS {}'.format(step, label_loss.item()))
+
+        # *************** save sample image after one epoch ***************
+        # save_images(cm_target[:, -1, ...].cpu(), cm_pred[:, -1, ...].cpu(),
+        #             epoch, img_name, save_dir)
+        #
+        # save_limb_images(limb_target[:, -1, ...].cpu(), limb_pred[:, -1, ...].cpu(),
+        #             epoch, img_name, save_dir)
+
+        # *************** eval model after one epoch ***************
+        eval_loss, cur_pck = eval(epoch, mode='valid')
+        logger.info('EPOCH {} VALID PCK  {}'.format(epoch, cur_pck))
+        logger.info('EPOCH {} TRAIN_LOSS {}'.format(epoch, sum(train_label_loss) / len(train_label_loss)))
+        logger.info('EPOCH {} VALID_LOSS {}'.format(epoch, eval_loss))
+
+        # *************** save current model and best model ***************
+        if cur_pck > max_pck:
+            torch.save(model.state_dict(), os.path.join(save_dir, 'best_model.pth'))
+            best_epo = epoch
+            max_pck = cur_pck
+        logger.info('Current Best EPOCH is : {}, PCK is : {}\n**************\n'.format(best_epo, max_pck))
+
+        # save current model
+        torch.save(model.state_dict(), os.path.join(save_dir, 'final_epoch.pth'))
+        # torch.save(model.state_dict(), os.path.join(save_dir, 'epoch_' + str(epoch) + '_' + str(cur_pck) + '.pth'))
+
+        # scheduler
+        scheduler.step(cur_pck)
+
+    logger.info('Train Done! ')
+    logger.info('Best epoch is {}'.format(best_epo))
+    logger.info('Best Valid PCK is {}'.format(max_pck))
+
+
+
+def eval(epoch, mode='valid'):
+    if mode == 'valid':
+        loader = valid_loader
+        gt_labels = valid_data.all_labels
+    else:
+        loader = test_loader
+        gt_labels = test_data.all_labels
+
+    with torch.no_grad():
+        all_pred_labels = {}        # save predict results
+        eval_loss = []
+        model.eval()
+        for step, (img, label_terget, img_name, w, h) in enumerate(loader):
+            if cuda:
+                img = img.cuda()
+            cm_pred = model(img)
+            # limb_pred (FloatTensor.cuda) size:(bz,3,C,46,46)
+            # cm_pred   (FloatTensor.cuda) size:(bz,3,21,46,46)
+
+            all_pred_labels = get_pred_coordinates(cm_pred.cpu(),
+                                                      img_name, w, h, all_pred_labels)
+            loss_final = loss.sum_mse_loss(cm_pred.cpu(), label_terget)
+            eval_loss.append(loss_final)
+
+        # ******** save predict labels for valid/test data ********
+        if mode == 'valid':
+            pred_save_dir = os.path.join(save_dir, 'e' + str(epoch) + '_val_pred.json')
+        else:
+            pred_save_dir = os.path.join(test_pck_dir, 'test_pred.json')
+        json.dump(all_pred_labels, open(pred_save_dir, 'w'), sort_keys=True, indent=4)
+
+        # ************* calculate and save PCKs  ************
+        pck_dict = get_pck_with_sigma(all_pred_labels, gt_labels, target_sigma_list)
+
+        if mode == 'valid':
+            pck_save_dir = os.path.join(save_dir, 'e' + str(epoch) + '_pck.json')
+        else:
+            pck_save_dir = os.path.join(test_pck_dir, 'pck.json')
+        json.dump(pck_dict, open(pck_save_dir, 'w'), sort_keys=True, indent=4)
+
+        select_pck = pck_dict[select_sigma]
+        eval_loss = sum(eval_loss)/len(eval_loss)
+    return eval_loss, select_pck
+
+
+
+train()
+
+logger.info('\nTESTING ============================>')
+logger.info('Load Trained model !!!')
+state_dict = torch.load(os.path.join(save_dir, 'best_model.pth'))
+model.load_state_dict(state_dict)
+eval(0, mode='test')
+
+logger.info('Done!')
+
+
+
+
